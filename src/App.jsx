c2b8e4e2df853653d5918b3a836a6030d6179f7a
Koalarix <koalarix96@gmail.com>
2024-12-16 15:16:04 +0800

    capstone-WIP
    WIP design (Css/JSX) , functionality not added yet

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './StockListsEmpty.jsx'
 function App() {
   return (
     <>
-    <div className='h-full'>
-      <div className="h-[16em] flex flex-col">
+    <div class="h-dvh w-full flex justify-center items-center border border-red-600">
+      <div className='w-[60%] min-w-[18em] max-w-[50em] max-h-[60em] rounded-[35px] bg-[#4a4e69] shadow-lg border border-yellow-300'>
+        <div className="h-[16em] flex flex-col">
           <Header />
           <Form/>
-      </div>
-      <div className="h-[35em]"> 
-        <StockDetails />
+        </div>
+          <StockDetails />
       </div>
     </div>
  
@@ -20,8 +20,9 @@ function App() {
 
 function Header() {
   return (
-    <header>
-    <h1 className='text-[2.3em] pt-6 pb-4 font-title font-bold text-white text-center'>Finance Dashboard</h1>
+    <header className="flex flex-col items-center">
+    <h1 className='w-full text-[1.25rem] [@media(min-width:750px)]:text-[2.3em] pt-6 pb-4 font-title font-bold text-white text-center'>Finance Dashboard</h1>
+    <hr className="w-[90%]"></hr>
     </header>
   )
 }
@@ -31,20 +32,24 @@ function Form() {
   <>
     <form  className='h-full flex flex-col items-center font-title font-light'>
 
-        <div  className="h-[5.5em] mt-2 flex justify-center items-center">
-          <input className="h-9 w-[10em] mx-2 pl-3 placeholder:italic" 
+        <div  className="h-[5.5em] mt-8 w-full flex flex-col justify-center items-center [@media(min-width:750px)]:flex-row">
+          <input className="h-9 w-[10em] min-w-0 mx-2 [@media(min-width:750px)]:ml-4 pl-3 placeholder:italic placeholder:text-[0.8em] [@media(min-width:750px)]:placeholder:text-[1em]" 
           type="text" id="stock" name="stock" placeholder="Stock Symbol"></input>
 
-          <input className=" h-9 w-[10em] mx-2 pl-3 placeholder:italic" 
+          <input className=" h-9 w-[10em] min-w-0 mx-2 pl-3 placeholder:italic placeholder:text-[0.8em] [@media(min-width:750px)]:placeholder:text-[1em]" 
           type="text" id="quantity" name="quantity" placeholder="Quantity" ></input>
 
-          <input className="h-9 w-[10em] mx-2 pl-3 placeholder:italic" 
+          <input className="h-9 w-[10em] min-w-0 mx-2 [@media(min-width:750px)]:mr-4 pl-3 placeholder:italic placeholder:text-[0.8em] [@media(min-width:750px)]:placeholder:text-[1em]" 
           type="text" id="price" name="price" placeholder="Purchase Price"></input> 
         </div>
+        <div className="size-full flex justify-center items-center">
 
-        <input className="h-10 w-[12em] font-bold text-white rounded-[5em] border-none bg-[#fca311] 
-        hover:border-none hover:bg-[#f19040ea] cursor-pointer" 
-        type="submit" value="Add Stock"/>
+          <input className="h-8 w-[8rem] text-[0.75rem] font-bold text-white rounded-[5em] bg-[#fca311] 
+          border-none hover:border-none hover:bg-[#f19040ea] cursor-pointer
+          [@media(min-width:750px)]:w-[12rem] [@media(min-width:750px)]:h-[38px] [@media(min-width:750px)]:text-[1rem]" 
+          type="submit" value="Add Stock"/>
+
+        </div>
 
     </form>
   </>
@@ -54,9 +59,11 @@ function Form() {
 function StockDetails() {
   return (
     <>
-      <section className="mt-8 flex flex-col items-center font-title"> 
-        <h2 className="text-[1.6em] text-white font-bold m-3">Stock List</h2>
-        <StockListsEmpty />  
+      <section className="font-title h-full"> 
+        <h2 className="text-[1.1rem] [@media(min-width:750px)]:text-[1.6em] w-full text-center text-white font-bold">Stock List</h2>
+        <div className="h-full flex justify-center">
+          <StockListsEmpty />
+        </div>  
       </section>
     </>
   )
@@ -65,8 +72,8 @@ function StockDetails() {
 
 function StockListsEmpty() {
   return (
-  <div className="bg-[#5e6381] w-[35em] h-[8em] text-center rounded-md border border-[#969ab6] flex justify-center items-center">
-    <p className="text-[#9aa0bb]">No stocks added yet.</p>
+  <div className="bg-[#5e6381] w-[80%] h-[12rem] mb-10 text-center rounded-md border border-[#969ab6] flex justify-center items-center">
+    <p className="text-[#9aa0bb] text-[0.75rem] [@media(min-width:750px)]:text-[1em]">No stocks added yet.</p>
   </div>
   )
 }
